e1cdb40a091c7069841c0171aec92b943ae8cd02
MVictoriaDoll <mvictoriadoll@gmail.com>
2024-06-22 22:10:25 +0200

    add more changes

--- src/js/main.js
+++ src/js/main.js
@@ -25,16 +25,28 @@ const getApiData = () => {
 // Paint Characters
 
 const getCharactersHtmlCode = (character) => {
+    
+   let imageUrl = '';
+  
+   if (character.imageUrl) {
+      imageUrl = character.imageUrl;  
+    } else {
+      character.imageUrl = `https://via.placeholder.com/210x295/ffffff/555555/?text=Disney`;
+    }
+    
     let htmlCode = '';
+
     htmlCode += `<article class="card">`;
     htmlCode += `<img src="${character.imageUrl}" class="card-img" alt="${character.name}"">`;
     htmlCode += `<h3 class= "card__name">${character.name} </h3>`;
     htmlCode += `</article>`;
+   
     return htmlCode;
 };
 
 const paintcharacters = () => {
   let charactersCode = '';
+  
   for (let character of characters){
     charactersCode += getCharactersHtmlCode(character);
   }
